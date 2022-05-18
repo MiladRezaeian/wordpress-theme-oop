b82ddcb98b0ffc4783192ebc43fafbaf356b6f12
Milad Rezaeian <milad.rezaeiann@gmail.com>
2022-05-19 02:09:26 +0430

    add classes autoloader and asset

---
+++ app/classes/class-asset.php
@@ -0,0 +1,21 @@
+<?php
+
+class Asset
+{
+
+    public static function css($file_name)
+    {
+
+    }
+
+    public static function js($file_name)
+    {
+
+    }
+
+    public static function image($file_name)
+    {
+
+    }
+
+}
