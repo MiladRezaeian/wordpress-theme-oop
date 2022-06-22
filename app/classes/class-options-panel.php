0c17657737f06f396d9e85e5a2f7331089f337ea
milad.rezaeiann@gmail.com <milad.rezaeiann@gmail.com>
2022-06-22 17:51:53 +0430

    theme options

--- app/classes/class-options-panel.php
+++ app/classes/class-options-panel.php
@@ -1,26 +1,45 @@
 <?php
 
-class Options_panel {
+class Options_panel
+{
 
-	public function __construct() {
-		$this->add_panel_menu();
-	}
+    public function __construct()
+    {
+        $this->add_panel_menu();
+        add_action('ooptheme_save_options_panel', array($this, 'save_options_handler'));
+    }
 
-	public function init_panel() {
-		add_action( 'admin_menu', array( $this, 'add_panel_menu' ) );
-	}
+    public function init_panel()
+    {
+        add_action('admin_menu', array($this, 'add_panel_menu'));
+    }
 
-	public function add_panel_menu() {
-		add_theme_page(
-			'theme setting',
-			'theme setting',
-			'manage_options',
-			'ooptheme_options_panel',
-			array($this,'panel_page')
-		);
-	}
+    public function add_panel_menu()
+    {
+        add_theme_page(
+            'theme setting',
+            'theme setting',
+            'manage_options',
+            'ooptheme_options_panel',
+            array($this, 'panel_page')
+        );
+    }
 
-	public function panel_page(  ) {
-		
-	}
+    public function panel_page()
+    {
+        if (isset($_POST['save_options'])) {
+            do_action('ooptheme_save_options_panel');
+        }
+        View::renderFile(admin . options_panel . index, array('options' => [1, 2]));
+    }
+
+    public function save_options_handler()
+    {
+
+    }
+
+    public static function load()
+    {
+        return get_option(THEME_OPTIONS);
+    }
 }
--- views/admin/options_panel/index.php
+++ views/admin/options_panel/index.php
@@ -1,3 +1,17 @@
 <div class="wrap">
 	<h1>OopTheme Setting</h1>
+    <style>
+        .form-row{
+            margin: 20px 0;
+        }
+    </style>
+    <form action="" method="post">
+        <div class="form-row">
+            <label for="member_content_active">Active access to content</label>
+            <input type="checkbox" name="member_content_active">
+        </div>
+        <div class="form-row">
+            <button type="submit" class="button button-primary" name="save_options">Save</button>
+        </div>
+    </form>
 </div>
