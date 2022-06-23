cb5efe0669357acf085a402c0e5f21aac0e2c49d
Milad <milad.rezaeiann@gmail.com>
2022-06-23 21:22:22 +0430

    check warning errors

--- app/classes/class-view.php
+++ app/classes/class-view.php
@@ -1,36 +1,31 @@
 <?php
 
-class View
-{
+class View {
 
-    public static function __callStatic($name, $arguments)
-    {
-        switch ($name) {
-            case 'render':
-                self::render_view($arguments[0]);
-                break;
-            case 'renderFile':
-                self::render_view_by_include($arguments[0], $arguments[1]);
-                break;
-        }
+	public static function __callStatic( $name, $arguments ) {
+		switch ( $name ) {
+			case 'render':
+				isset( $arguments[1] ) ? self::render_view( $arguments[0], $arguments[1] ) : self::render_view( $arguments[0] );
+				break;
+			case 'renderFile':
+				isset( $arguments[1] ) ? self::render_view_by_include( $arguments[0], $arguments[1] ) : self::render_view_by_include( $arguments[0] );
+				break;
+		}
 
-    }
+	}
 
-    private static function render_view($view_name)
-    {
+	private static function render_view( $view_name ) {
+		get_template_part( 'views/' . $view_name );
+	}
 
-        get_template_part('views/' . $view_name);
-    }
-
-    public static function render_view_by_include($view_name, $data = null)
-    {
-        $view_name = str_replace('.', DS, $view_name);
-        $view_file_path = THEME_VIEW . DS . $view_name . '.php';
-        if (is_file($view_file_path) && is_readable($view_file_path)) {
-            !empty($data) ? extract($data) : null;
-            include $view_file_path;
-        }
-    }
+	public static function render_view_by_include( $view_name, $data = null ) {
+		$view_name      = str_replace( '.', DS, $view_name );
+		$view_file_path = THEME_VIEW . DS . $view_name . '.php';
+		if ( is_file( $view_file_path ) && is_readable( $view_file_path ) ) {
+			! empty( $data ) ? extract( $data ) : null;
+			include $view_file_path;
+		}
+	}
 
 
 }
